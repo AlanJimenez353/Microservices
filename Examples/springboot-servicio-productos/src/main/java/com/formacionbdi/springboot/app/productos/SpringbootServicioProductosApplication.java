6687f18a2927aae7c9da05bb8a44cb9cd37eaf90
Alan dev <Alan.jimenez353@gmail.com>
2023-09-06 22:33:15 -0300

    Added eureka server

--- Examples/springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ Examples/springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,9 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
